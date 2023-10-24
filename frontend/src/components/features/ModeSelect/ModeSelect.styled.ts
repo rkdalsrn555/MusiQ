04e34f9bd16389cba02d6beb4ad1a01f5ee282b1
ping9oo <fpdl96@gmail.com>
2023-10-24 10:43:11 +0900

    S09P31A608-344 Feat: 모드 선택 컴포넌트 구현

--- frontend/src/components/features/ModeSelect/ModeSelect.styled.ts
+++ frontend/src/components/features/ModeSelect/ModeSelect.styled.ts
@@ -2,24 +2,40 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const Wrapper = styled(motion.div)`
-height: 100vh;
-width: 100vw;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  overflow: hidden;
 `;
 
 export const Box = styled(motion.div)`
-width: 400px;
-height: 200px;
-background-color: rgba(255, 255, 255, 1);
-border-radius: 40px;
-position: absolute;
-top: 150px;
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: 28px;
-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  width: 40rem;
+  height: 27rem;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 2.5rem;
+  position: absolute;
+  top: 9.375rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.75rem;
+  box-shadow:
+    0 0.125rem 0.1875rem rgba(0, 0, 0, 0.1),
+    0 0.625rem 1.25rem rgba(0, 0, 0, 0.06);
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  bottom: 4rem;
+  position: absolute;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 20rem; // 버튼 간격을 조절
+`;
+
+export const RotatedImage = styled.img`
+  transform: rotate(180deg);
 `;
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,2 +1,3 @@
 export * from './Landing';
 export * from './ModeSelectPage/ModeSelectPage';
+export * from './SingleModePage/SingleModePage';
