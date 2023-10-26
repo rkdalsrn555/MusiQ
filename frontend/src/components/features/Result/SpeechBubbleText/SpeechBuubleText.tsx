6c055dc2d2b35874e22a5eff1aa596b21ce0b8c4
CHAERYEON-LEE <sogogijjang@naver.com>
2023-10-26 17:23:30 +0900

    S09P31A608-352 Feat: 결과 페이지 완성

--- frontend/src/components/features/Result/SpeechBubbleText/SpeechBuubleText.tsx
+++ frontend/src/components/features/Result/SpeechBubbleText/SpeechBuubleText.tsx
@@ -1,9 +1,16 @@
+import { FC } from 'react';
 import { StyledBubble, StyledFont } from './SpeechBubbleText.styled';
 
-export const SpeechBubbleText = () => (
+interface SpeechBubbleTextProps {
+  correctAnswerCnt: number;
+}
+
+export const SpeechBubbleText: FC<SpeechBubbleTextProps> = ({
+  correctAnswerCnt,
+}) => (
   <StyledBubble>
-    <StyledFont>축하해! 총 10문제를 맞췄어.</StyledFont>
-    <StyledFont>친구들과 결과를 공유하면,</StyledFont>
-    <StyledFont>친구들을 조롱할 수 있어!!</StyledFont>
+    <StyledFont>축하해! 총 {correctAnswerCnt}문제를 맞췄어.</StyledFont>
+    <StyledFont>친구들과 결과를 공유하고,</StyledFont>
+    <StyledFont>기록을 자랑해봐!!!</StyledFont>
   </StyledBubble>
 );
