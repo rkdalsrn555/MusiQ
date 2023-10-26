a21cc0ecea227275d2c3870cbe1d8db0e44bb712
ping9oo <fpdl96@gmail.com>
2023-10-26 10:56:27 +0900

    S09P31A608-353 Feat: 결과 페이지 구현 중

--- frontend/src/components/features/Result/ResultInfo/ResultInfo.styled.ts
+++ frontend/src/components/features/Result/ResultInfo/ResultInfo.styled.ts
@@ -1,5 +1,61 @@
 import styled from 'styled-components';
 
-export const InfroWrapper = styled.div`
+export const InfoWrapper = styled.div`
+  width: 35rem;
+  height: 42rem;
+  flex-shrink: 0;
+  border: solid 5px rgba(235, 226, 255, 0.4);
+  border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 5rem;
+  margin-top: 10rem;
+`;
 
-`
+export const ChickWrapper = styled.div`
+  width: 35rem;
+  height: 42rem;
+  flex-shrink: 0;
+  display: flex;
+  margin-top: 10rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DancingChickContainer = styled.div`
+  position: relative;
+  width: 29rem;
+  height: 20rem;
+  top: 10%;
+
+  & img:nth-child(1) {
+    position: absolute;
+    top: 10%;
+    bottom: 0;
+    left: 12%;
+    right: 0;
+    z-index: 2;
+  }
+
+  & img:nth-child(2) {
+    position: absolute;
+    top: 70%;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
+
+  & img:nth-child(3) {
+    position: absolute;
+    bottom: 50%;
+    left: 30%;
+    right: 0;
+  }
+`;
--- frontend/src/components/features/Result/ResultInfo/ResultInfo.tsx
+++ frontend/src/components/features/Result/ResultInfo/ResultInfo.tsx
@@ -1,5 +1,24 @@
-export const ResultInfo = () => {
-  <div>
-    hi
-  </div>
-}
+import { FC } from 'react';
+import {
+  InfoWrapper,
+  ChickWrapper,
+  ResultContainer,
+  DancingChickContainer,
+} from './ResultInfo.styled';
+import { SpeechBubbleText } from '../SpeechBubbleText';
+import DanceChick from '../../../../assets/img/playgame/danceChick.gif';
+import WaveBar from '../../../../assets/img/playgame/wave.png';
+import SpeechBubble from '../../../../assets/img/playgame/speechBubble.png';
+
+export const ResultInfo = () => (
+  <ResultContainer>
+    <ChickWrapper>
+      <DancingChickContainer>
+        <img src={DanceChick} alt="DanceChick" width={300} />
+        <img src={WaveBar} alt="WaveBar" width={400} />
+        <img src={SpeechBubble} alt="SpeechBubble" width={400} />
+      </DancingChickContainer>
+    </ChickWrapper>
+    <InfoWrapper>this is wrapper</InfoWrapper>
+  </ResultContainer>
+);
