9d6bf2d5a93c36e9437c551f0b1754414baa3dd8
ping9oo <fpdl96@gmail.com>
2023-10-30 21:12:42 +0900

    S09P31A608-383 Feat: 모바일 접근 제한 페이지 구현, 모드 선택 가이드 대사 및 css 수정

---
+++ frontend/src/pages/MobilePage/MobilePage.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledMobileDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh; 
+`
