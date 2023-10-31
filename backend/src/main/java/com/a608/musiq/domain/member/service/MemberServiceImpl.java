02c00e6f7fddf54b299a65aa7b0bd31b8696f84d
Chae Huichan <eqholic1125@gmail.com>
2023-10-31 17:54:06 +0900

    S09P31A608-518 Feat: 방문자수 저장 기능 구현

--- backend/src/main/java/com/a608/musiq/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/a608/musiq/domain/member/service/MemberServiceImpl.java
@@ -2,8 +2,11 @@ package com.a608.musiq.domain.member.service;
 
 import org.springframework.stereotype.Service;
 
+import com.a608.musiq.domain.member.domain.Visitor;
+import com.a608.musiq.domain.member.dto.VisitResponseDto;
 import com.a608.musiq.domain.member.repository.MemberInfoRepository;
 import com.a608.musiq.domain.member.repository.MemberRepository;
+import com.a608.musiq.domain.member.repository.VisitorRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +16,12 @@ public class MemberServiceImpl implements MemberService {
 
 	private final MemberRepository memberRepository;
 	private final MemberInfoRepository memberInfoRepository;
+	private final VisitorRepository visitorRepository;
 
+	@Override
+	public VisitResponseDto visit(String userIp) {
+		visitorRepository.save(Visitor.of(userIp));
+
+		return VisitResponseDto.of(userIp);
+	}
 }
