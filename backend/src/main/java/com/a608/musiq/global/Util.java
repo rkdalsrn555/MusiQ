fd13832eedf9cee184700b16c5f29c18a359e2bb
ghwhdthd@naver.com <ghwhdthd@naver.com>
2023-11-02 10:23:01 +0900

    S09P31A608-539 Feat: 랭킹 삭제 메서드를 구현하기 위한 Util 클래스 생성

---
+++ backend/src/main/java/com/a608/musiq/global/Util.java
@@ -0,0 +1,16 @@
+package com.a608.musiq.global;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Util {
+
+
+    public void deleteRankingInRedis(){
+
+    }
+    public void insertRankingInRedis(){
+
+    }
+
+}
