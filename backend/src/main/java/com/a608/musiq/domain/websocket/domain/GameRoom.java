a4438a6b472aedc55a18865f845344053916f9c6
rkdalsrn314 <rkdalsrn314@gmail.com>
2023-11-06 18:09:30 +0900

    S09P31A608-481 Feat: Websocket Domain 작성 - ChatMessage 작성 - GameRoom 작성 - UserInfoItem 작성

---
+++ backend/src/main/java/com/a608/musiq/domain/websocket/domain/GameRoom.java
@@ -0,0 +1,26 @@
+package com.a608.musiq.domain.websocket.domain;
+
+import com.a608.musiq.domain.websocket.data.GameRoomType;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GameRoom {
+    private String roomName;
+    private String password;
+    private int totalUsers;
+    private GameRoomType gameRoomType;
+    private int round;
+    private int skipVote;
+    private List<UserInfoItem> userInfoItemList;
+}
