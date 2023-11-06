72b97f384fc09ec0ee59efce551094afc9f406a2
ping9oo <fpdl96@gmail.com>
2023-11-06 17:09:07 +0900

    S09P31A608-532 Feat: 랭킹 페이지 구현 완료

--- frontend/src/components/features/Ranking/RankingInfo.tsx
+++ frontend/src/components/features/Ranking/RankingInfo.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { BackBtn } from '../../utils/BackBtn/BackBtn';
 import {
   RankingWrapper,
@@ -11,58 +12,134 @@ import {
   Cell,
   HeaderCell,
   RankingContainer,
+  StyledMyRanking,
+  PrizeImage,
+  PrizeCell,
+  MyRankingGrade,
+  MyRankingText,
+  LoginButton,
 } from './Ranking.styled';
-import rankingLogo from '../../../assets/svgs/rankingIcon.svg';
+import rankingLogo from '../../../assets/svgs/ranking/rankingIcon.svg';
+import crownImg from '../../../assets/svgs/ranking/Crown.svg';
+import silverImg from '../../../assets/svgs/ranking/silverCrown.svg';
+import bronzeImg from '../../../assets/svgs/ranking/bronzeCrown.svg';
+import loginLogo from '../../../assets/svgs/login/loginKey.svg';
 
-// const fetchRankingData = async () => {
-//   try {
-//     const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/ranking`);
-//     return response.data.map(([nickname, exp], index) => ({
-//       rank: index + 1,
-//       nickname,
-//       exp
-//     }));
-//   } catch (error) {
-//     console.error('Error fetching ranking data:', error);
-//     return [];
-//   }
-// };
+type RankingData = {
+  rankNum: number;
+  nickName: string;
+  exp: number;
+};
 
-const rankingData = Array.from({ length: 100 }, (_, index) => ({
-  rank: index + 1,
-  nickname: `User${index + 1}`,
-  exp: Math.floor(Math.random() * 1000), // 임의의 랜덤값, api 완성되면 바꿔야함
-}));
+type ApiResponse = {
+  code: number;
+  message: string;
+  data: {
+    rankList: RankingData[];
+    myRank: string;
+  };
+};
 
-export const RankingInfo = () => (
-  <CenteredContainer>
-    <BackBtn url="/select-mode" />
-    <RankingLogoImg src={rankingLogo} alt="Ranking Logo" width={400} />
-    <RankingWrapper>
-      <RankingContainer>
-        <RankingHeader>
-          <HeaderCell>순위</HeaderCell>
-          <HeaderCell>닉네임</HeaderCell>
-          <HeaderCell>경험치</HeaderCell>
-        </RankingHeader>
-        <RankingItemsWrapper>
-          {rankingData.map(item => (
-            <RankingItem key={item.rank}>
-              <Cell>{item.rank}</Cell>
-              <Cell>{item.nickname}</Cell> 
-              <Cell>{item.exp}</Cell> 
-            </RankingItem>
-          ))}
-        </RankingItemsWrapper>
-      </RankingContainer>
-    </RankingWrapper>
-  </CenteredContainer>
-);
+const nickname = localStorage.getItem('nickname');
+const MyRanking = ({ rankNum }: { rankNum: string | number | null }) => {
+  const navigate = useNavigate();
 
-// const [rankingData, setRankingData] = useState([]); // 이건 RankingInfo 안에 넣을거임
+  const handleLoginRedirect = () => {
+    navigate('/login');
+  };
 
-//   useEffect(() => {
-//     fetchRankingData().then(data => {
-//       setRankingData(data);
-//     });
-//   }, [])
+  return (
+    <StyledMyRanking>
+      {nickname === null ? (
+        <>
+          <div
+            style={{
+              fontSize: '22px',
+              fontWeight: 'bold',
+              marginBottom: '10%',
+            }}
+          >
+            로그인하고 내 랭킹 확인하기
+          </div>
+          <LoginButton onClick={handleLoginRedirect}>
+            <img src={loginLogo} alt="로그인 페이지로 이동" width={70} />
+            <div style={{ fontWeight: 'bold', fontSize: '20px' }}>Login</div>
+          </LoginButton>
+        </>
+      ) : (
+        <>
+          <MyRankingText>나의 순위는?</MyRankingText>
+          <MyRankingGrade>
+            {Number.isNaN(Number(rankNum)) ? rankNum : `${rankNum}위`}
+          </MyRankingGrade>
+        </>
+      )}
+    </StyledMyRanking>
+  );
+};
+
+export const RankingInfo: React.FC = () => {
+  const [rankingData, setRankingData] = useState<RankingData[]>([]);
+  const [myRank, setMyRank] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchRankingData = async () => {
+      try {
+        const response = await axios.get<ApiResponse>(
+          `${process.env.REACT_APP_BASE_URL}/ranking/fullranking?nickname=${nickname}`
+        );
+        console.log(nickname);
+        setRankingData(response.data.data.rankList);
+        setMyRank(response.data.data.myRank);
+        setLoading(false);
+      } catch (error) {
+        console.error('랭킹 데이터를 가져오는 중 에러가 발생했습니다:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchRankingData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <CenteredContainer>
+      <BackBtn url="/select-mode" />
+      <RankingLogoImg src={rankingLogo} alt="Ranking Logo" width={400} />
+      <RankingWrapper>
+        <RankingContainer>
+          <RankingHeader>
+            <HeaderCell>순위</HeaderCell>
+            <HeaderCell>닉네임</HeaderCell>
+            <HeaderCell>경험치</HeaderCell>
+          </RankingHeader>
+          <RankingItemsWrapper>
+            {rankingData.map((item) => (
+              <RankingItem key={item.rankNum}>
+                <PrizeCell>
+                  {item.rankNum === 1 && (
+                    <PrizeImage src={crownImg} alt="1등!" width={30} />
+                  )}
+                  {item.rankNum === 2 && (
+                    <PrizeImage src={silverImg} alt="2등!" width={30} />
+                  )}
+                  {item.rankNum === 3 && (
+                    <PrizeImage src={bronzeImg} alt="3등!" width={30} />
+                  )}
+                </PrizeCell>
+                <Cell>{item.rankNum}</Cell>
+                <Cell>{item.nickName}</Cell>
+                <Cell>{item.exp}</Cell>
+              </RankingItem>
+            ))}
+          </RankingItemsWrapper>
+        </RankingContainer>
+      </RankingWrapper>
+      <MyRanking rankNum={myRank} />
+    </CenteredContainer>
+  );
+};
