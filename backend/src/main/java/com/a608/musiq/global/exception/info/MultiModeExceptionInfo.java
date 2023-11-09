bc7fef0f4728f8bbfd747890ce7c6e032c2e54e9
rkdalsrn314 <rkdalsrn314@gmail.com>
2023-11-09 10:09:57 +0900

    S09P31A608-481 Feat: CustomException 추가 - MultiModeExceptionInfo 추가

--- backend/src/main/java/com/a608/musiq/global/exception/info/MultiModeExceptionInfo.java
+++ backend/src/main/java/com/a608/musiq/global/exception/info/MultiModeExceptionInfo.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum MultiModeExceptionInfo {
-    INVALID_JOIN_REQUEST(HttpStatus.BAD_REQUEST, 1600, "정원 초과인 채널입니다.");
+    INVALID_JOIN_REQUEST(HttpStatus.BAD_REQUEST, 1600, "정원 초과인 채널입니다."),
+    OUT_OF_ROOM_NUMBER(HttpStatus.BAD_REQUEST, 1601, "현재 생성할 수 있는 방이 없습니다.");
 
     private final HttpStatus status;
     private final Integer code;
