0336a1cf8335cf2d78417eb175999d055dd1e668
CHAERYEON-LEE <sogogijjang@naver.com>
2023-11-11 22:27:15 +0900

    S09P31A608-611 Feat: 소켓 채팅 완료

--- frontend/src/components/features/Channel/ChannelComponent.tsx
+++ frontend/src/components/features/Channel/ChannelComponent.tsx
@@ -1,50 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// eslint-disable-next-line import/no-unresolved
-import { Client, Stomp } from '@stomp/stompjs';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { websocketClientState } from '../../../atoms/atoms';
-
-import { userApis } from '../../../hooks/api/userApis';
 import { ChannelItemsWrapper, ChannelItem } from './ChannelComponent.styled';
 
-export const ChannelComponent = () => {
-  const setWebsocketClient = useSetRecoilState(websocketClientState);
-  const [channelSizes, setChannelSizes] = useState([]);
-  const navigate = useNavigate();
-  const accessToken = window.localStorage.getItem('userAccessToken');
+type OwnProps = {
+  channelSizes: number[];
+  handleChannelClick: (channelNumber: number) => void;
+};
 
-  const handleChannelClick = async (channelNumber: number) => {
-    navigate(`/multi/${channelNumber}/lobby`);
-  };
-
-  useEffect(() => {
-    if (!accessToken) {
-      console.error('Access token is not available.');
-      return;
-    }
-
-    const fetchChannelSizes = async () => {
-      try {
-        const response = await userApis.get(
-          `${process.env.REACT_APP_BASE_URL}/game/channel`,
-          {
-            headers: {
-              accessToken,
-            },
-          }
-        );
-        if (response.status === 200) {
-          setChannelSizes(response.data.data.channelSizes);
-        }
-      } catch (error) {
-        console.error('공습경보', error);
-      }
-    };
-
-    fetchChannelSizes();
-  }, []);
+export const ChannelComponent = (props: OwnProps) => {
+  const { channelSizes, handleChannelClick } = props;
 
   return (
     <ChannelItemsWrapper>
@@ -53,9 +15,8 @@ export const ChannelComponent = () => {
         return (
           <ChannelItem
             key={`channel-${channelNumber}`}
-            onClick={async () => {
-              // 웹소켓 객체 생성
-              await handleChannelClick(channelNumber);
+            onClick={() => {
+              handleChannelClick(channelNumber);
             }}
           >
             <p
--- frontend/src/pages/SocketPage/SocketPage.tsx
+++ frontend/src/pages/SocketPage/SocketPage.tsx
@@ -2,42 +2,88 @@ import React, { useRef, useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 // eslint-disable-next-line import/no-unresolved
 import * as StompJs from '@stomp/stompjs';
+import { useLocation } from 'react-router-dom';
+import { userApis } from '../../hooks/api/userApis';
 import { MultiGamePlaying, MultiGameLobbyPage } from '..';
 
 export const SocketPage = () => {
-  const [isMakeRoom, setIsMakeRoom] = useState<boolean>(false);
+  const accessToken = window.localStorage.getItem('userAccessToken') ?? '';
+  const [isRoomExisted, setIsRoomExisted] = useState<boolean>(false);
+  const location = useLocation();
   const client = useRef<any>({});
+  const topicNumber = useRef<number>(0);
+  // 로비 채팅 리스트 상태 - 병철
+  const [lobbyChatList, setLobbyChatList] = useState<
+    { nickname: string; message: string }[]
+  >([]);
 
   // 구독
   const subscribe = () => {
-    client.current.subscribe('/topic/{채널넘버}');
-    client.current.subscribe('/chat-message/{로비 채널넘버}');
-    client.current.subscribe('/chat-message/{게임방 채널넘버}');
+    client.current.subscribe(
+      `/topic/${topicNumber.current}`,
+      (message: any) => {
+        // 채널넘버일때 - 병철
+        if (topicNumber.current >= 1 || topicNumber.current <= 10) {
+          const msg = JSON.parse(message.body);
+          setLobbyChatList((prev) => [
+            ...prev,
+            { nickname: msg.nickname, message: msg.message },
+          ]);
+        } else {
+          // 게임방 관련 상태 - 채련
+          console.log(message);
+        }
+      }
+    );
   };
 
   // 소켓 연결
   const connect = () => {
     client.current = new StompJs.Client({
-      brokerURL: 'ws://localhost:8080/api',
+      brokerURL: 'ws://localhost:8080/api/game-websocket',
+      connectHeaders: {
+        accessToken,
+        channelNo: location.pathname.split('/')[2],
+      },
       onConnect: subscribe,
+      onStompError: (frame) => {
+        console.error('STOMP Error:', frame.headers.message);
+      },
     });
     client.current.activate();
   };
 
   // 소켓 연결 해제
+  const postSocketDisconnect = async () => {
+    try {
+      const response = await userApis.post(
+        `${process.env.REACT_APP_BASE_URL}/game/${Number(
+          location.pathname.split('/')[2]
+        )}`
+      );
+      console.log('Left channel successfully.', response.data);
+    } catch (error) {
+      console.error('Error leaving channel:', error);
+    }
+  };
+
   const disconnect = () => {
     client.current.deactivate();
   };
 
   // 첫 렌더링 시 소켓연결, 페이지 떠날 시 disconnect
   useEffect(() => {
+    topicNumber.current = Number(location.pathname.split('/')[2]);
     connect();
-    return () => disconnect();
+    return () => {
+      disconnect();
+      postSocketDisconnect();
+    };
   }, []);
 
   return (
     <div>
-      {isMakeRoom ? (
+      {isRoomExisted ? (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -53,7 +99,10 @@ export const SocketPage = () => {
           exit={{ opacity: 0 }}
           transition={{ duration: 1 }}
         >
-          <MultiGameLobbyPage />
+          <MultiGameLobbyPage
+            socketClient={client}
+            lobbyChatList={lobbyChatList}
+          />
         </motion.div>
       )}
     </div>
