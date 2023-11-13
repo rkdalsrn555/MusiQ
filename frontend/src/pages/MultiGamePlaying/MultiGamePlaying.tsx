9e2496809cdb2fec415180c4f3a468373b96dc96
CHAERYEON-LEE <sogogijjang@naver.com>
2023-11-13 17:54:04 +0900

    S09P31A608-611 Feat: 멀티모드 게임현황 구현 및 메세지타입 미리 작성

--- frontend/src/pages/MultiGamePlaying/MultiGamePlaying.tsx
+++ frontend/src/pages/MultiGamePlaying/MultiGamePlaying.tsx
@@ -13,18 +13,33 @@ import {
 } from '../../components/features';
 import * as S from './MultiGamePlaying.styled';
 
+type GameUserList = {
+  nickname: string;
+  score: number;
+};
+
 type GameChatType = {
   nickname: string;
   message: string;
 };
 
+const mockUserData1 = [
+  { score: 12, nickname: '장충동왕족발보쌈' },
+  { score: 6, nickname: '이르케' },
+];
+
 export const MultiGamePlaying = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const accessToken = window.localStorage.getItem('userAccessToken') ?? '';
-  const client = useRef<any>({});
-  const [gameChatList, setGameChatList] = useState<GameChatType[]>([]);
-  const gameRoomNumber = Number(location.pathname.split('/')[4]);
+  const client = useRef<any>({}); // 게임 소켓 클라이언트
+  const gameRoomNumber = Number(location.pathname.split('/')[4]); // 게임방번호
+  const [gameChatList, setGameChatList] = useState<GameChatType[]>([]); // 채팅리스트
+  const [gameUserList, setGameUserList] = useState<GameUserList[]>([]); // 유저리스트
+
+  const [manager, setManager] = useState<string>(''); // 내가 게임방의 매니저인지 아닌지
+
+  const [playTime, setPlayTime] = useState<number>(0); // 플레이타임
 
   useEffect(() => {
     // 모바일 기기 접근을 막는 로직
@@ -41,10 +56,65 @@ export const MultiGamePlaying = () => {
   const subscribe = () => {
     client.current.subscribe(`/topic/${gameRoomNumber}`, (message: any) => {
       const msg = JSON.parse(message.body);
-      setGameChatList((prev) => [
-        ...prev,
-        { nickname: msg.nickname, message: msg.message },
-      ]);
+
+      switch (msg.messageType) {
+        case 'ENTERUSER': // 유저 입장 시 마다 pub
+          setGameUserList(mockUserData1);
+          setManager('장충동왕족발보쌈');
+          setGameChatList((prev) => [
+            ...prev,
+            {
+              nickname: '삐약이',
+              message: `${msg.enteredUserNickname}님이 입장하셨습니다.`,
+            },
+          ]);
+          break;
+        case 'EXITUSER': // 유저 나갈 때 pub
+          setGameUserList(mockUserData1);
+          setManager(msg.manager);
+          setGameChatList((prev) => [
+            ...prev,
+            {
+              nickname: '삐약이',
+              message: `${msg.enteredUserNickname}님이 퇴장하셨습니다.`,
+            },
+            {
+              nickname: '삐약이',
+              message: `방장이 ${msg.manager}님으로 변경되었습니다. 방장이 게임을 시작해주세요.`,
+            },
+          ]);
+          break;
+        case 'CHAT': // 유저가 채팅 보냈을 때
+          setGameChatList((prev) => [
+            ...prev,
+            { nickname: msg.nickname, message: msg.message },
+          ]);
+          break;
+        case 'GAMESTART': // 게임 시작 버튼 클릭시
+          break;
+        case 'TIME': // 시간초세기
+          break;
+        case 'MUSICPROBLEM': // 음악 문제 세팅
+          break;
+        case 'SINGERHINT': // 가수힌트
+          break;
+        case 'INITIALHINT': // 초성힌트
+          break;
+        case 'GAMERESULT': // 게임 끝났을 때 유저리스트 반환
+          break;
+        case 'BEFORESKIP': // 누군가 문제 맞추기 전 스킵요청
+          break;
+        case 'AFTERSKIP': // 문제 맞춘 후 스킵요청
+          break;
+        case 'BEFOREANSWERCORRECT': // 정답 맞췄을때 누가 정답맞췄고, 정답이 뭔지
+          break;
+        case 'MUSICPLAY': // 노래 시작 타이밍
+          break;
+        case 'GOWAITING': // 게임 끝났을 때 대기상태로 다시 변환
+          break;
+        default:
+          break;
+      }
     });
   };
 
@@ -73,6 +143,8 @@ export const MultiGamePlaying = () => {
   // 첫 렌더링 시 소켓연결, 페이지 떠날 시 disconnect
   useEffect(() => {
     connect();
+    setGameUserList(mockUserData1);
+    setManager('장충동왕족발보쌈');
     return () => {
       disconnect();
     };
@@ -86,8 +158,9 @@ export const MultiGamePlaying = () => {
       transition={{ duration: 1 }}
     >
       <S.Container>
-        <MultiGameStatus />
+        <MultiGameStatus gameUserList={gameUserList} manager={manager} />
         <MultiDancingChick />
+        <div>게임 대기중입니다</div>
         <MultiGameHint />
         <MultiGameSkip />
         <MultiGameChatting
