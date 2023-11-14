bcda336a8253e698880ce22b52a8b32abba345d2
Chae Huichan <eqholic1125@gmail.com>
2023-11-15 01:27:29 +0900

    S09P31A608-688 Chore: GameRoom 미사용 메서드 삭제

--- backend/src/main/java/com/a608/musiq/domain/websocket/domain/GameRoom.java
+++ backend/src/main/java/com/a608/musiq/domain/websocket/domain/GameRoom.java
@@ -88,11 +88,6 @@ public class GameRoom {
         this.skipVote = skipVote;
     }
 
-    public void setUserInfoItems(
-        Map<UUID, UserInfoItem> userInfoItems) {
-        this.userInfoItems = userInfoItems;
-    }
-
     public void changeGameRoomType(GameRoomType type) {
         this.gameRoomType = type;
     }
