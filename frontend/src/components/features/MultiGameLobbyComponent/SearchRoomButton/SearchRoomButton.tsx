965efd5e7d1abd895cce083c6bc62d82ec3afa76
ping9oo <fpdl96@gmail.com>
2023-11-14 23:48:04 +0900

    S09P31A608-659 Feat: 방 찾기 기능 구현 중

--- frontend/src/components/features/MultiGameLobbyComponent/SearchRoomButton/SearchRoomButton.tsx
+++ frontend/src/components/features/MultiGameLobbyComponent/SearchRoomButton/SearchRoomButton.tsx
@@ -1,3 +1,101 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userApis } from '../../../../hooks/api/userApis';
+import {
+  ModalOverlay,
+  ModalContent,
+  CloseButton,
+} from './SearchRoomButton.styled';
 
-export const SearchRoomButton = () => <div>방 찾기 버튼 만들거임</div>;
+interface ModalProps {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ children, onClose }) => (
+  <ModalOverlay onClick={onClose}>
+    <ModalContent onClick={(e) => e.stopPropagation()}>
+      {children}
+      <CloseButton onClick={onClose}>&times;</CloseButton>
+    </ModalContent>
+  </ModalOverlay>
+);
+
+interface SearchRoomButtonProps {
+  channelNumber: string;
+}
+
+export const SearchRoomButton: React.FC<SearchRoomButtonProps> = ({
+  channelNumber,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [roomNumber, setRoomNumber] = useState('');
+  const navigate = useNavigate();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleInputChange = (e: any) => {
+    setRoomNumber(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const roomsResponse = await userApis.get(
+        `${process.env.REACT_APP_BASE_URL}/game/main/${channelNumber}`
+      );
+      // eslint-disable-next-line prefer-destructuring
+      const rooms = roomsResponse.data.data.rooms;
+      console.log(roomNumber);
+      const selectedRoom = rooms.find(
+        (room: any) => room.gameRoomNo === parseInt(roomNumber, 10)
+      );
+
+      if (!selectedRoom) {
+        alert('해당 번호의 방이 없습니다.');
+        return;
+      }
+
+      const enterResponse = await userApis.get(
+        `${process.env.REACT_APP_BASE_URL}/game/main/enter/${selectedRoom.gameRoomNo}`
+      );
+
+      if (enterResponse.data.code === 200) {
+        const gameState = {
+          channelNo: parseInt(channelNumber, 10),
+        };
+        navigate(`/multi/${channelNumber}/game/${selectedRoom.gameRoomNo}`, {
+          state: { requestBody: gameState },
+        });
+      }
+    } catch (error) {
+      console.error('Error entering room:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpenModal}>
+        방 찾기
+      </button>
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <input
+            type="text"
+            placeholder="방 번호 입력"
+            value={roomNumber}
+            onChange={handleInputChange}
+          />
+          <button type="button" onClick={handleSubmit}>
+            입장
+          </button>
+        </Modal>
+      )}
+    </div>
+  );
+};
