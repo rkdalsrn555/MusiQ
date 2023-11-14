3fa2a71b0c2db7bc7abefc904e3a7f865e8cb94b
CHAERYEON-LEE <sogogijjang@naver.com>
2023-11-15 02:31:11 +0900

    S09P31A608-611 Feat: 멀티모드 구현

--- frontend/src/components/features/MultiGamePlay/MultiGameChatting/MultiGameChatting.tsx
+++ frontend/src/components/features/MultiGamePlay/MultiGameChatting/MultiGameChatting.tsx
@@ -15,6 +15,7 @@ type ChatType = {
 };
 
 type OwnProps = {
+  userLength: number;
   skipVote: number;
   gameChatList: ChatType[];
   socketClient: React.MutableRefObject<any>;
@@ -22,7 +23,8 @@ type OwnProps = {
 };
 
 export const MultiGameChatting = (props: OwnProps) => {
-  const { skipVote, gameChatList, socketClient, setGameChatList } = props;
+  const { userLength, skipVote, gameChatList, socketClient, setGameChatList } =
+    props;
   const accessToken = window.localStorage.getItem('userAccessToken');
   const location = useLocation();
   const focusRef = useRef<HTMLInputElement>(null);
@@ -30,6 +32,7 @@ export const MultiGameChatting = (props: OwnProps) => {
   const inputFocusRef = useRef<boolean>(false);
   const [inputText, setInputText] = useState<string>('');
   const chatEndRef = useRef<HTMLDivElement>(null); // 채팅창 맨 밑으로 스크롤 내리기
+  const nickname = window.localStorage.getItem('nickname');
 
   const sendChat = () => {
     if (String(inputTextRef.current).trim() === '') {
@@ -48,6 +51,15 @@ export const MultiGameChatting = (props: OwnProps) => {
         nickname: window.localStorage.getItem('nickname'),
       }),
     });
+    // if (inputTextRef.current === '.') {
+    //   setGameChatList((prev) => [
+    //     ...prev,
+    //     {
+    //       nickname: '삐약이',
+    //       message: `${nickname}님 스킵투표되었습니다, 이 투표는 다른사람에게 공개되지 않습니다.`,
+    //     },
+    //   ]);
+    // }
     setInputText('');
   };
 
@@ -87,7 +99,7 @@ export const MultiGameChatting = (props: OwnProps) => {
         inputFocusRef.current = false;
       }}
     >
-      <MultiSkipBox skipVote={skipVote} />
+      <MultiSkipBox skipVote={skipVote} userLength={userLength} />
       <S.ChatListContainer>
         {gameChatList.map((chat, index) => (
           <S.NicknameColor
